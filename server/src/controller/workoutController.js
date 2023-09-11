f3d314262e9d71947db007c8a0adf6abe28da4a4
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-11 23:46:05 +0300

    check for valid id in `getWorkout()`

--- server/src/controller/workoutController.js
+++ server/src/controller/workoutController.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require('mongoose');
 const Workout = require('../models/Workout.model')
 
 const getWorkouts = async (req, res) => {
@@ -16,6 +17,11 @@ const deleteWorkouts = async (req, res) => {
 const getWorkout = async (req, res) => {
     const { id } = req.params;
     try {
+        if (!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(404).json({
+                invalidIdErr: 'no such workout data found.'
+            });
+        }
         const workout = await Workout.findById(id);
         if (workout) {
             res.status(200).json(workout);
