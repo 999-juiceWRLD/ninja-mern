12c98dd2e806e212ee60e2ad14943794117979d7
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-11 23:09:08 +0300

    make imports look nicer and set up the routes better

--- server/src/routes/workouts.js
+++ server/src/routes/workouts.js
@@ -1,9 +1,22 @@
 const express = require('express')
-const { getWorkouts, getWorkout, postWorkout } = require('../controller/workoutController')
+const { getWorkouts,
+        deleteWorkouts,
+        getWorkout,
+        postWorkout,
+        deleteWorkout,
+        patchWorkout } = require('../controller/workoutController')
 const router = express.Router();
 
 router
-    .get('/', getWorkouts);
+    .route('/')
+    .get(getWorkouts)
+    .delete(deleteWorkouts)
 
+router
+    .route('/:id')
+    .get(getWorkout)
+    .post(postWorkout)
+    .delete(deleteWorkout)
+    .patch(patchWorkout)
 
-module.exports = { router };
+module.exports = router;
