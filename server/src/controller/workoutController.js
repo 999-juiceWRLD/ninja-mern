4dfb242041bd7981fc7b375fc851254c220ace25
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-11 23:04:33 +0300

    create empty controller functions, will update later

---
+++ server/src/controller/workoutController.js
@@ -0,0 +1,26 @@
+const getWorkouts = async (req, res) => {
+    await res.json({ message: "get all workouts"});
+}
+
+const getWorkout = async (req, res) => {
+    const { id } = req.body;
+}
+
+const postWorkout = async (req, res) => {
+
+}
+
+const deleteWorkout = async (req, res) => {
+    const { id } = req.body;
+
+}
+
+const patchWorkout = async (req, res) => {
+    const { id } = req.body;
+}
+
+module.exports = {
+    getWorkouts,
+    getWorkout,
+    postWorkout
+}
