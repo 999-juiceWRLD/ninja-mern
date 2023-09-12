d6709860ff5e4655e0f2433fa29207979c3f8d01
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-12 19:46:24 +0300

    if db is empty, return relevant message. otherwise map through all the data fetched

--- client/src/pages/Home.component.jsx
+++ client/src/pages/Home.component.jsx
@@ -1,8 +1,31 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+import { WorkoutDetails } from "../components/WorkoutDetails.component"
 import axios from "axios"
 
 export const Home = () => {
     
+    const [workouts, setWorkouts] = useState([]);
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const traverseArrIfEmpty = (array) => {
+        if (workouts.length === 0) {
+            return (<NoElement />) 
+        } else if (workouts.length > 0) {
+            return array.map((e, idx) => (
+                <WorkoutDetails 
+                    key={idx}
+                    workout={e}
+                />
+            ));
+        }
+    }
+
+    const NoElement = () => {
+        return (
+            <p>{errorMessage}</p>
+        );
+    }
+
     useEffect(() => {
         const fetch = async () => {
             await axios.get('http://localhost:3000/api/workouts')
@@ -14,7 +37,9 @@ export const Home = () => {
                 .then(data => {
                     if (data.workouts) {
                         console.log(data.workouts);
+                        setWorkouts(data.workouts)
                     } else {
+                        setErrorMessage(data['noContentErr']);
                         console.log(data);
                     }
                 })
@@ -26,6 +51,7 @@ export const Home = () => {
     return (
         <div className="home">
             <h1>Home</h1>
+            {traverseArrIfEmpty(workouts)}
         </div>
     );
 }
