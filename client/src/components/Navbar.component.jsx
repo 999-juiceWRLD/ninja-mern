2ed69f5d4e6909b6ed98a3ae98283021e39b85b4
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-13 00:56:12 +0300

    wrap the previous code inside a `<header />` tag

--- client/src/components/Navbar.component.jsx
+++ client/src/components/Navbar.component.jsx
@@ -2,10 +2,12 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
     return (
-        <div className="container">
-            <Link to={'/'}>
-                <h1>Workout Buddy</h1>
-            </Link>
-        </div>
+        <header>
+            <div className="container">
+                <Link to={'/'}>
+                    <h1>Workout Buddy</h1>
+                </Link>
+            </div>
+        </header>
     );
 }
