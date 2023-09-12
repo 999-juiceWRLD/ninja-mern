72e1fba856eb5ed37bdd980e221eb6e991058018
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-13 02:43:17 +0300

    add uniqueness to the `title`

--- server/src/models/Workout.model.js
+++ server/src/models/Workout.model.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose')
 const workoutSchema = new mongoose.Schema({
     title: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     reps: {
         type: Number,
