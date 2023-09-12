f20f2f389fe6db202714d710c616a58c63b9eed5
juice wrld <ankaraunifedaisi@gmail.com>
2023-09-12 18:17:41 +0300

    fetch data from db

--- client/src/pages/Home.component.jsx
+++ client/src/pages/Home.component.jsx
@@ -1,5 +1,20 @@
+import { useEffect } from "react";
+import axios from "axios"
+
 export const Home = () => {
+    
+    useEffect(() => {
+        const fetch = async () => {
+            await axios.get('http://localhost:3000/api/workouts')
+                .then(res => { console.log(res) })
+                .catch(err => { console.log(err)})
+        }
+        fetch();
+        }, [])
+
     return (
-        <div>ayo wassup</div>
+        <div className="home">
+            <h1>Home</h1>
+        </div>
     );
 }
